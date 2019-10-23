2bc447f8ecfdecead8741d0c11c4d9aa7b4504e9
giao@lango <giao@etancesys.com>
2019-10-23 15:20:53 +0200

    fix: email widget: python scripts moved to the correct directory

--- email-widget/email.lua
+++ email-widget/email.lua
@@ -6,24 +6,27 @@ local watch = require("awful.widget.watch")
 local path_to_icons = "/usr/share/icons/Arc/actions/22/"
 
 email_widget = wibox.widget.textbox()
--- email_widget:set_font('Play 9')
+email_widget:set_font('Play 9')
 
 email_icon = wibox.widget.imagebox()
 email_icon:set_image(path_to_icons .. "/mail-mark-new.png")
 
 watch(
-    --"python /home/giao/.config/awesome/email-widget/count_unread_emails_xdays.py", 20,
-    "python /home/giao/.config/awesome/email-widget/count_unread_emails.py", 300,
+    --"python /home/giao/.config/awesome/awesome-wm-widgets/email-widget/count_unread_emails_xdays.py", 20,
+    "python /home/giao/.config/awesome/awesome-wm-widgets/email-widget/count_unread_emails.py", 300,
     function(widget, stdout, stderr, exitreason, exitcode)
-        local unread_emails_num = tonumber(stdout) or 0
+        local unread_emails_num = tonumber(stdout) or -1
         if (unread_emails_num > 0) then
         	--email_icon:set_image(path_to_icons .. "/mail-mark-unread.png")
         	email_icon:set_image(path_to_icons .. "star.png")
 	        email_widget:set_text(stdout)
         elseif (unread_emails_num == 0) then
-        	--email_icon:set_image(path_to_icons .. "/mail-message-new.png")
+        	email_icon:set_image(path_to_icons .. "/mail-message-new.png")
+        	--email_icon:set_image(path_to_icons .. "exit.png")
+   	      email_widget:set_text("")
+        else
         	email_icon:set_image(path_to_icons .. "exit.png")
-   	        email_widget:set_text("")
+   	      email_widget:set_text("cant access mail server ?")
         end	
     end
 )
